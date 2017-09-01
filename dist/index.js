05637d11d775e0527c57c2fc8958ecea09dd72b8
Logan Barnett <logustus@gmail.com>
2017-08-31 17:42:11 -0700

    include remaining build tools and actually make the build

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,18 @@
 'use strict';
 
-import fs from 'fs';
-import printFlowDefinition from 'css-modules-flow-types-printer';
-import Tokenizer from 'css-selector-tokenizer';
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
+var _cssModulesFlowTypesPrinter = require('css-modules-flow-types-printer');
+
+var _cssModulesFlowTypesPrinter2 = _interopRequireDefault(_cssModulesFlowTypesPrinter);
+
+var _cssSelectorTokenizer = require('css-selector-tokenizer');
+
+var _cssSelectorTokenizer2 = _interopRequireDefault(_cssSelectorTokenizer);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // function getTokens(content) {
 //   const cssTokens = content.replace(/\s/g, '').match('exports.locals=(.*);');
@@ -35,10 +45,10 @@ module.exports = function cssModulesFlowTypesLoader(content) {
   // NOTE: We cannot use .emitFile as people might use this with devServer
   // (e.g. in memory storage).
   const outputPath = this.resourcePath + '.flow';
-  const cssContents = fs.readFileSync(this.resourcePath, 'utf8');
-  const nodes = getAllClassNames(Tokenizer.parse(cssContents));
-  const output = printFlowDefinition(toNamesObj(nodes));
-  fs.writeFile(outputPath, output, {}, function () {});
+  const cssContents = _fs2.default.readFileSync(this.resourcePath, 'utf8');
+  const nodes = getAllClassNames(_cssSelectorTokenizer2.default.parse(cssContents));
+  const output = (0, _cssModulesFlowTypesPrinter2.default)(toNamesObj(nodes));
+  _fs2.default.writeFile(outputPath, output, {}, function () {});
 
   return content;
 };
